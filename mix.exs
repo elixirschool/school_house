004cd84f02e6912be4651d35b166fe18f13ad627
Sean Callan <seancallan@gmail.com>
2021-02-10 17:41:52 -0700

    Get tests passing

--- mix.exs
+++ mix.exs
@@ -67,8 +67,7 @@ defmodule SchoolHouse.MixProject do
     [
       setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-      "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      "ecto.reset": ["ecto.drop", "ecto.setup"]
     ]
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,1 @@
 ExUnit.start()
-Ecto.Adapters.SQL.Sandbox.mode(SchoolHouse.Repo, :manual)
