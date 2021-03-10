ea25fe02e853f7a892560ed4aa6e09b73ead10a2
Sean Callan <sean@system76.com>
2021-03-10 13:29:09 -0700

    Add AlpineJS

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,12 +13,13 @@ import "../css/app.css"
 //     import socket from "./socket"
 //
 import "phoenix_html"
-import {Socket} from "phoenix"
+import { Socket } from "phoenix"
 import NProgress from "nprogress"
-import {LiveSocket} from "phoenix_live_view"
+import { LiveSocket } from "phoenix_live_view"
+import 'alpinejs'
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken } })
 
 // Show progress bar on live navigation and form submits
 window.addEventListener("phx:page-loading-start", info => NProgress.start())
