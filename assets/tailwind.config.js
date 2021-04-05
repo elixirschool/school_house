56ab50dd74b782a936cd215eb049aa3b07965a88
Sean Callan <sean@system76.com>
2021-04-04 19:15:21 -0600

    Continue iterating on UI

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,65 +1,66 @@
 module.exports = {
-  purge: [
-    '../lib/ground_control_web/**/*.ex',
-    '../lib/ground_control_web/**/*.leex',
-    '../lib/ground_control_web/**/*.eex',
-    './js/**/*.js'
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        'brand-light-gray': '#f4f6f7',
-        'brand-gray': '#3c4349',
-        'brand-purple': '#7c6f89',
-        'brand-light-purple': '#cfbae6',
-        'brand-red': '#c0394d',
-      },
-      container: {
-        center: true,
-      },
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            color: theme('colors.brand-gray'),
-            maxWidth: 'inherit',
-            pre: {
-              'background-color': '#f4f5f6',
-              color: '#5a6267'
+    purge: [
+        '../lib/ground_control_web/**/*.ex',
+        '../lib/ground_control_web/**/*.leex',
+        '../lib/ground_control_web/**/*.eex',
+        './js/**/*.js'
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                'brand-light-gray': '#f4f6f7',
+                'brand-gray': '#3c4349',
+                'brand-purple': '#7c6f89',
+                'brand-light-purple': '#cfbae6',
+                'brand-red': '#c0394d',
             },
-            h1: {
-              color: theme('colors.brand-gray'),
+            container: {
+                center: true,
             },
-            h2: {
-              color: theme('colors.brand-gray'),
-            },
-            h3: {
-              color: theme('colors.brand-gray'),
-            },
-            a: {
-              color: theme('colors.brand-purple'),
-              '&:hover': {
-                'background-color': theme('colors.brand-purple'),
-                color: theme('colors.white'),
-              }
-            },
-            'ul li': {
-              marginTop: '0px',
-              marginBottom: '0px',
-            },
-            'ul ul': {
-              marginTop: '0px',
-              marginBottom: '0px',
-            },
-          }
-        }
-      }),
+            typography: (theme) => ({
+                DEFAULT: {
+                    css: {
+                        color: theme('colors.brand-gray'),
+                        maxWidth: 'inherit',
+                        pre: {
+                            'background-color': '#f4f5f6',
+                            color: '#5a6267'
+                        },
+                        h1: {
+                            color: theme('colors.brand-gray'),
+                        },
+                        h2: {
+                            color: theme('colors.brand-gray'),
+                        },
+                        h3: {
+                            color: theme('colors.brand-gray'),
+                        },
+                        a: {
+                            color: theme('colors.brand-purple'),
+                            '&:hover': {
+                                'background-color': theme('colors.brand-purple'),
+                                color: theme('colors.white'),
+                            }
+                        },
+                        'ul li': {
+                            marginTop: '0px',
+                            marginBottom: '0px',
+                        },
+                        'ul ul': {
+                            marginTop: '0px',
+                            marginBottom: '0px',
+                        },
+                    }
+                }
+            }),
+        },
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/typography')
-  ],
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms'),
+    ],
 }
