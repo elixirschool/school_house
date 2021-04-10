306b6b20095d18b253343b111406828efb7c1679
Sean Callan <sean@system76.com>
2021-04-10 14:38:27 -0600

    Setup heroku deploy

---
+++ config/releases.exs
@@ -0,0 +1,25 @@
+import Config
+
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
+live_view_salt =
+  System.get_env("LIVE_VIEW_SALT") ||
+    raise """
+    environment variable LIVE_VIEW_SALT is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
+config :school_house, GroundControlWeb.Endpoint,
+  http: [
+    port: String.to_integer(System.get_env("PORT") || "4000"),
+    transport_options: [socket_opts: [:inet6]]
+  ],
+  secret_key_base: secret_key_base,
+  live_view: [signing_salt: live_view_salt],
+  url: [scheme: "https", host: "beta.elixirschool.com"],
+  server: true
