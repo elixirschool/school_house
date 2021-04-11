58817f964df3af263d402329e416eca7787e8d29
Sean Callan <sean@system76.com>
2021-04-11 17:52:16 -0600

    Dont force SSL in Phoenix

--- config/prod.exs
+++ config/prod.exs
@@ -11,8 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :school_house, SchoolHouseWeb.Endpoint,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :debug
