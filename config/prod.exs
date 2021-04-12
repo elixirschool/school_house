ca7f16d40cf0e096e22885a24f3cb0f39ea10dfb
Sean Callan <sean@system76.com>
2021-04-11 18:36:54 -0600

    Production log level should be info

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ config :school_house, SchoolHouseWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :debug
+config :logger, level: :info
 
 # ## SSL Support
 #
