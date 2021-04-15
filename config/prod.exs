fdc51839eb75cff52c5107fee7481893bd1f421f
Sean Callan <doomspork@users.noreply.github.com>
2021-04-15 10:21:18 -0600

    Update config/prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :school_house, SchoolHouseWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "beta.elixirschool.com", port: 443]
+  url: [scheme: "https", host: "beta.elixirschool.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
