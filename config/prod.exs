7982414cb6d182b76212fdc9c68b1c29b0a1225b
Jaime Iniesta <cuentas@jaimeiniesta.com>
2021-04-15 18:24:32 +0200

    configures production as explained in guides for Heroku (#23)
    * configures production as explained in guides for Heroku
    
    * Update config/prod.exs
    
    * Update config/prod.exs
    
    Co-authored-by: Sean Callan <doomspork@users.noreply.github.com>

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :school_house, SchoolHouseWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "beta.elixirschool.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
