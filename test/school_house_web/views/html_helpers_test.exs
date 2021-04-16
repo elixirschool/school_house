76390f59cb5da23ce64a46493bafb78faa8c1dae
Sean Callan <doomspork@users.noreply.github.com>
2021-04-16 08:07:50 -0600

    We only need to modify the path (#25)
    * We only need to modify the path
    
    Update the function to `_url` from `_link` to more accurately reflect
    what it does. Only modify the request path, the entire url is not
    necessary. Fixes #22.
    
    * Move setup to top level

---
+++ test/school_house_web/views/html_helpers_test.exs
@@ -0,0 +1,49 @@
+defmodule SchoolHouseWeb.HtmlHelpersTest do
+  use SchoolHouseWeb.ConnCase
+
+  alias SchoolHouseWeb.HtmlHelpers
+
+  setup do
+    Gettext.put_locale("es")
+
+    on_exit(fn ->
+      Gettext.put_locale("en")
+    end)
+  end
+
+  describe "avatar_url/1" do
+    test "appends .png to GitHub profile link" do
+      assert "https://github.com/elixirschool.png" == HtmlHelpers.avatar_url("https://github.com/elixirschool")
+    end
+  end
+
+  describe "current_locale/0" do
+    test "returns the current locale used by Gettext" do
+      assert "es" == HtmlHelpers.current_locale()
+    end
+  end
+
+  describe "current_page_locale_path/2" do
+    test "returns the current page path for another locale" do
+      assert "/fr/ecto/changesets" ==
+               :get
+               |> build_conn("/es/ecto/changesets")
+               |> HtmlHelpers.current_page_locale_path("fr")
+
+      # With or without a leading slash, the replacement is correct
+      assert "fr/ecto/changesets" ==
+               :get
+               |> build_conn("es/ecto/changesets")
+               |> HtmlHelpers.current_page_locale_path("fr")
+    end
+  end
+
+  describe "supported_locales/0" do
+    test "returns a list of all supported locales" do
+      locales = HtmlHelpers.supported_locales()
+
+      assert is_list(locales)
+      assert 1 < length(locales)
+    end
+  end
+end
