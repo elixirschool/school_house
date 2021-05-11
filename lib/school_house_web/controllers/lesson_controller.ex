25302ee237eb611d4df18e1d5f0e03c495c07623
Sean Callan <doomspork@users.noreply.github.com>
2021-05-10 18:08:04 -0600

    Add new page for listing lessons per section (#41)

--- lib/school_house_web/controllers/lesson_controller.ex
+++ lib/school_house_web/controllers/lesson_controller.ex
@@ -6,6 +6,11 @@ defmodule SchoolHouseWeb.LessonController do
 
   action_fallback FallbackController
 
+  def index(conn, %{"section" => section}) do
+    lessons = Lessons.list(section, Gettext.get_locale(SchoolHouseWeb.Gettext))
+    render(conn, "index.html", lessons: lessons, section: section)
+  end
+
   def lesson(conn, %{"name" => name, "section" => section}) do
     with {:ok, lesson} <- Lessons.get(section, name, Gettext.get_locale(SchoolHouseWeb.Gettext)) do
       render(conn, "lesson.html", lesson: lesson)
--- lib/school_house_web/controllers/page_controller.ex
+++ lib/school_house_web/controllers/page_controller.ex
@@ -8,20 +8,21 @@ defmodule SchoolHouseWeb.PageController do
   end
 
   def privacy(conn, _params) do
-    render(conn, "privacy.html")
+    render(conn, "privacy.html", page_title: "Privacy Policy")
   end
 
   def report(conn, %{"locale" => locale}) do
-    render(conn, "report.html", report: Lessons.translation_report(locale))
+    render(conn, "report.html", page_title: "Translation Report", report: Lessons.translation_report(locale))
   end
 
   def why(conn, _params) do
-    render(conn, "why.html")
+    render(conn, "why.html", page_title: "Why Choose Elixir?")
   end
 
   defp recent_posts do
-    0
-    |> Posts.page()
-    |> Enum.take(6)
+    # 0
+    # |> Posts.page()
+    # |> Enum.take(6)
+    []
   end
 end
