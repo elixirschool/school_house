c42bc7c5c8af9d6fa39450b1210b83820f6367e8
kinson <swilkinsonhunter@gmail.com>
2021-05-20 17:51:09 -0600

    more tweaks for the lesson menu, locale menu, etc

--- lib/school_house_web/views/html_helpers.ex
+++ lib/school_house_web/views/html_helpers.ex
@@ -19,7 +19,7 @@ defmodule SchoolHouseWeb.HtmlHelpers do
 
   def friendly_version({major, minor, patch}), do: "#{major}.#{minor}.#{patch}"
 
-  def lesson_link(conn, section, name, class \\ "block hover:bg-brand-purple text-brand-gray hover:text-white",
+  def lesson_link(conn, section, name, class \\ "block hover:bg-brand-purple-800 hover:dark:bg-brand-purple-200 text-brand-gray-750 dark:text-gray-200 hover:text-white hover:dark:text-brand-gray",
         do: contents
       ) do
     {destination, additional_classes} =
