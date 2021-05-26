c4081d745cfc273530e59e285d660c560be0864d
kinson <swilkinsonhunter@gmail.com>
2021-05-26 11:49:21 -0600

    format the html helpers file

--- lib/school_house_web/views/html_helpers.ex
+++ lib/school_house_web/views/html_helpers.ex
@@ -19,7 +19,11 @@ defmodule SchoolHouseWeb.HtmlHelpers do
 
   def friendly_version({major, minor, patch}), do: "#{major}.#{minor}.#{patch}"
 
-  def lesson_link(conn, section, name, class \\ "block hover:bg-purple hover:dark:bg-purple text-primary dark:text-primary-dark hover:text-white hover:dark:text-light-dark",
+  def lesson_link(
+        conn,
+        section,
+        name,
+        class \\ "block hover:bg-purple hover:dark:bg-purple text-primary dark:text-primary-dark hover:text-white hover:dark:text-light-dark",
         do: contents
       ) do
     {destination, additional_classes} =
