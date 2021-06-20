cc3b6421deacb8b32ffdd89902eafdc77a23193b
Sam Hunter <swilkinsonhunter@gmail.com>
2021-06-19 19:07:51 -0600

    Improve Responsiveness (#65)
    * Make the Nav Into a Collapsable Menu for Smaller Viewports
    
    Updated the nav element to be a collapsable menu when the
    viewport is small (as in mobile, tablet, etc), so that
    users can still access all of the menu bar links from
    smaller screens.
    
    * more support for dark mode
    
    * fix the lesson menu responsiveness
    
    * separate out js logic into separate files
    
    * better html formatting
    
    * update responsiveness on other pages
    
    * a few more spacing tweaks

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,43 +33,5 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)
 window.liveSocket = liveSocket
 
-// set up dark mode toggle
-function setDarkMode(on) {
-    if (on) {
-        darkModeToggle.checked = true
-        document.documentElement.classList.add('dark')
-        localStorage.theme = 'dark'
-        document.getElementById('sun-icon').classList.add('hidden')
-        document.getElementById('moon-icon').classList.remove('hidden')
-    } else {
-        darkModeToggle.checked = false
-        document.documentElement.classList.remove('dark')
-        localStorage.theme = 'light'
-        document.getElementById('moon-icon').classList.add('hidden')
-        document.getElementById('sun-icon').classList.remove('hidden')
-    }
-}
-
-const darkModeToggle = document.getElementById("dark-mode-toggle")
-
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    setDarkMode(true)
-} else {
-    setDarkMode(false)
-}
-
-darkModeToggle.addEventListener('click', function (ev) {
-    if (ev.target.checked) {
-        setDarkMode(true)
-    } else {
-        setDarkMode(false)
-    }
-})
-
-// remove preload class after the page laods so the styles
-// will transition smoothly when switching between dark and
-// light mode. Without the preload class, the transition will
-// happen on page load if dark mode is enabled
-setTimeout(() => {
-    document.body.classList.remove('preload')
-}, 200)
+import "./dark-mode"
+import "./menu-toggle"
