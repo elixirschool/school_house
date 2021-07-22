4b3cbeb6728be69db8688f01832493d472fa777a
Jaime Iniesta <jaimeiniesta@gmail.com>
2021-07-22 09:03:30 +0200

    moves ConferenceLive out of controllers folder

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,6 @@
 ExUnit.start()
 
-defmodule SchoolHouseWeb.ConferenceLive.IndexTestHelpers do
+defmodule SchoolHouseWeb.ConferenceLive.TestHelpers do
   import Phoenix.LiveViewTest
 
   def apply_filter(view, filters) do
