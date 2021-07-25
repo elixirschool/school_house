8376de14fa4b1d0c7d790e75c1b86d75b94ab074
Jaime Iniesta <jaimeiniesta@gmail.com>
2021-07-25 17:39:56 +0200

    fixes invalid meta tag for csrf token
    phoenix_html generates invalid HTML code for this meta tag. This is going to be fixed in 3.0
    but in the meantime we change it in our layout to fix our most common HTML issue on the site.
    
    https://github.com/phoenixframework/phoenix_html/issues/294
    
    Fixes #94

--- lib/school_house_web.ex
+++ lib/school_house_web.ex
@@ -37,7 +37,7 @@ defmodule SchoolHouseWeb do
 
       # Import convenience functions from controllers
       import Phoenix.Controller,
-        only: [get_flash: 1, get_flash: 2, view_module: 1, view_template: 1]
+        only: [get_csrf_token: 0, get_flash: 1, get_flash: 2, view_module: 1, view_template: 1]
 
       # Include shared imports and aliases for views
       unquote(view_helpers())
