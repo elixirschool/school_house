6390b5989506c4d3a3fd75cfaf8100f68e89ee08
kinson <swilkinsonhunter@gmail.com>
2021-07-29 18:47:41 -0600

    Add Sitemap Links for Dark Mode
    Added another sitemap file for dark mode
    so that Rocket Validator can validate
    accessibility of dark mode as well.

--- lib/school_house_web/views/layout_view.ex
+++ lib/school_house_web/views/layout_view.ex
@@ -1,3 +1,10 @@
 defmodule SchoolHouseWeb.LayoutView do
   use SchoolHouseWeb, :view
+
+  def get_dark_mode_param(conn) do
+    case Map.get(conn.query_params, "ui", nil) do
+      "dark" -> "dark"
+      _ -> ""
+    end
+  end
 end
