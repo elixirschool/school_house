a807bc4113bfc9600f708e8f603575dc64a05a7f
kinson <swilkinsonhunter@gmail.com>
2021-09-24 21:42:57 -0600

    Upgrade to Phoenix 1.6.0

--- lib/school_house_web/router.ex
+++ lib/school_house_web/router.ex
@@ -40,25 +40,4 @@ defmodule SchoolHouseWeb.Router do
       get "/lessons/:section/:name", LessonController, :lesson
     end
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", SchoolHouseWeb do
-  #   pipe_through :api
-  # end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: SchoolHouseWeb.Telemetry
-    end
-  end
 end
