07669ee9805f75f9b93baaa07cae6c48a6dc5480
thth <ththfh@gmail.com>
2021-10-06 21:00:24 -0700

    fix header regex not recognizing bengali, tamil

--- lib/school_house/content/lesson.ex
+++ lib/school_house/content/lesson.ex
@@ -3,7 +3,7 @@ defmodule SchoolHouse.Content.Lesson do
   Encapsulates an individual lesson and handles parsing the originating markdown file
   """
 
-  @headers_regex ~r/<(h\d)>(["\p{L}\s?!\.\/\d]+)(?=<\/\1>)/iu
+  @headers_regex ~r/<(h\d)>(["\p{L}\p{M}\s?!\.\/\d]+)(?=<\/\1>)/iu
   @enforce_keys [:body, :section, :locale, :name, :title, :version]
 
   defstruct [
