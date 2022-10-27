7a6e873bb4cd53e7fb6a8ff14e3684e30ea9c20d
Marius Kluften <mkluften93@gmail.com>
2022-10-27 22:26:21 +0200

    Default to Device Preference for Site Theme (#207)
    * 🎨 Improve dark mode to rely on the following order: localstorage > device prefs
    
    * ✏️ Fix a simple typo

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -1,24 +1,36 @@
-const darkModeToggleContainer = document.getElementById("dark-mode-toggle-container")
-const darkModeToggleInput = document.getElementById("dark-mode-toggle")
+const darkModeToggleContainer = document.getElementById(
+    'dark-mode-toggle-container'
+)
+const darkModeToggleInput = document.getElementById('dark-mode-toggle')
 
 // set up dark mode toggle
 function setDarkMode(on) {
     if (on) {
         darkModeToggleInput.checked = true
         document.documentElement.classList.add('dark')
-        localStorage.theme = 'dark'
         document.getElementById('sun-icon').classList.add('hidden')
         document.getElementById('moon-icon').classList.remove('hidden')
     } else {
         darkModeToggleInput.checked = false
         document.documentElement.classList.remove('dark')
-        localStorage.theme = 'light'
         document.getElementById('moon-icon').classList.add('hidden')
         document.getElementById('sun-icon').classList.remove('hidden')
     }
 }
 
-if (document.documentElement.classList.contains('dark')) {
+// Will prefer dark mode, if the user has set it on their device.
+const userPrefersDarkMode =
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches
+
+// If the user has taken an active choice to set mode, which is stored
+// in local storage, use that. Otherwise, prefer user device preference.
+if (localStorage.theme) {
+    setDarkMode(localStorage.theme === 'dark')
+} else if (
+    userPrefersDarkMode ||
+    document.documentElement.classList.contains('dark')
+) {
     setDarkMode(true)
 } else {
     setDarkMode(false)
@@ -26,8 +38,10 @@ if (document.documentElement.classList.contains('dark')) {
 
 darkModeToggleContainer.addEventListener('click', function () {
     if (darkModeToggleInput.checked) {
+        localStorage.theme = 'light'
         setDarkMode(false)
     } else {
+        localStorage.theme = 'dark'
         setDarkMode(true)
     }
 })
